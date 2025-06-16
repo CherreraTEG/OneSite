113fe0c1ec5d52e0e9a7551beb60fc7b086fa8c4
Claudia Johana Herrera Torres <cherrera@eliteflower.com>
2025-06-16 17:10:45 -0400

    feat: Configurar módulo de autenticación con Active Directory

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,27 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'permisos',
+        loadChildren: () => import('./modules/permisos/permisos.module').then(m => m.PermisosModule),
+        data: { roles: ['admin', 'supervisor'] }
+      },
+      {
+        path: 'costos',
+        loadChildren: () => import('./modules/costos/costos.module').then(m => m.CostosModule),
+        data: { roles: ['admin', 'supervisor', 'usuario'] }
+      },
+      {
+        path: 'camiones',
+        loadChildren: () => import('./modules/camiones/camiones.module').then(m => m.CamionesModule),
+        data: { roles: ['admin', 'supervisor'] }
+      },
+      {
+        path: 'auditoria',
+        loadChildren: () => import('./modules/auditoria/auditoria.module').then(m => m.AuditoriaModule),
+        data: { roles: ['admin'] }
+      }
+    ]
   },
   {
-    path: 'permisos',
-    loadChildren: () => import('./modules/permisos/permisos.module').then(m => m.PermisosModule)
+    path: 'unauthorized',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: 'costos',
-    loadChildren: () => import('./modules/costos/costos.module').then(m => m.CostosModule)
-  },
-  {
-    path: 'camiones',
-    loadChildren: () => import('./modules/camiones/camiones.module').then(m => m.CamionesModule)
-  },
-  {
-    path: 'auditoria',
-    loadChildren: () => import('./modules/auditoria/auditoria.module').then(m => m.AuditoriaModule)
+    path: '**',
+    redirectTo: ''
   }
 ];
 
---
+++ frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    TranslateModule,
+    SharedModule
+  ],
+  exports: [
+    LoginComponent
+  ]
+})
+export class AuthModule { } 
